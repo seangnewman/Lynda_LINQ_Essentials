46004a9e515746a7d0fafda67b6040ea290e484f
newmanuevers <seangnewman@msn.com>
2018-10-23 07:22:16 -0700

    Course Completion

---
+++ 09_02_Querying_Trees_start/Start_LINQ_XML/09_02_Querying_Trees_start.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Start_LINQ_XML
+{
+    class Start_LINQ_XML
+    {
+
+        static void Main(string[] args)
+        {
+            var custQuery =
+                from c in XDocument.Load("Customers.xml").Descendants("Customer")
+                where (c.Element("State").Value) == "OR"
+                select c.Element("First").Value;
+
+            foreach (var customer in custQuery)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
